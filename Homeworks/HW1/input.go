183593865e7ccf3f82d74a14bb056883dc5f8a5e
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-11-17 15:37:09 +0200

    Update input.go

--- Homeworks/HW1/input.go
+++ Homeworks/HW1/input.go
@@ -3,8 +3,35 @@ package main
 import (
 	"bufio"
 	"os"
-	"strings"
 )
+
+// return true if the input byte string representation is equal to [' ', '\t', '\n', '\r', '\f', '\v']
+func isSpace(inputByte byte) bool {
+	switch inputByte {
+	case ' ', '\t', '\n', '\r', '\f', '\v':
+		return true
+	default:
+		return false
+	}
+}
+
+// removes all the whitespaces in the start and in the end
+func trimSpace(inputString string) string {
+	startIndex := 0
+	endIndex := len(inputString)
+
+	// loop for whitespaces in the front
+	for startIndex < endIndex && isSpace(inputString[startIndex]) {
+		startIndex += 1
+	}
+
+	// loop for whitespaces in the back
+	for endIndex > startIndex && isSpace(inputString[endIndex-1]) {
+		endIndex -= 1
+	}
+
+	return inputString[startIndex:endIndex]
+}
 
 func ReadUsernamesFromFile(path string) ([]string, error) {
 	file, err := os.Open(path)
@@ -16,7 +43,8 @@ func ReadUsernamesFromFile(path string) ([]string, error) {
 	scanner := bufio.NewScanner(file)
 	var usernames []string
 	for scanner.Scan() {
-		line := strings.TrimSpace(scanner.Text())
+		line := trimSpace(scanner.Text())
+		// to avoid empty lines
 		if line != "" {
 			usernames = append(usernames, line)
 		}
