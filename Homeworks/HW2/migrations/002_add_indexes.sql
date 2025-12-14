1e98dd7e6aa86a952dfba9035245cad3e320a3c5
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-12-14 23:26:43 +0200

    Update 002_add_indexes.sql

--- Homeworks/HW2/migrations/002_add_indexes.sql
+++ Homeworks/HW2/migrations/002_add_indexes.sql
@@ -1,7 +1,7 @@
-CREATE INDEX idx_images_created_at ON images(created_at);
-CREATE INDEX idx_images_format_created ON images(format, created_at);
-CREATE INDEX idx_images_width ON images(width);
-CREATE INDEX idx_images_height ON images(height);
+CREATE INDEX IF NOT EXISTS idx_images_created_at ON images(created_at);
+CREATE INDEX IF NOT EXISTS idx_images_format_created ON images(format, created_at);
+CREATE INDEX IF NOT EXISTS idx_images_width ON images(width);
+CREATE INDEX IF NOT EXISTS idx_images_height ON images(height);
 
-CREATE INDEX idx_images_url_prefix ON images(url(255));
-CREATE INDEX idx_images_filename_prefix ON images(filename(255));
+CREATE INDEX IF NOT EXISTS idx_images_url_prefix ON images(url(255));
+CREATE INDEX IF NOT EXISTS idx_images_filename_prefix ON images(filename(255));
